5d41a563d7a2ec4dd13b6923fdabb0b512dab6b9
Mostafa Abdelsalam <abdelslam.mostafa@gmail.com>
2020-07-29 17:26:46 +0200

    Adding the app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './containers/App.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Hello greeting = {'Hello' + 'React Ninja'}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+  <App />
+
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
